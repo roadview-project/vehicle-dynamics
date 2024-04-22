0247bd77078452ef764faf1386ed7b959c6091bc
Yuri <42354569+poledna@users.noreply.github.com>
2024-04-22 14:15:42 +0200

    update on Main to work with the new pickle

--- main.py
+++ main.py
@@ -6,52 +6,66 @@ ROADVIEW Project
 Funded by the European Union (grant no. 101069576). Views and opinions expressed are however those of the author(s) only and do not necessarily reflect those of the European Union or the European Climate, Infrastructure and Environment Executive Agency (CINEA). Neither the European Union nor the granting authority can be held responsible for them. UK and Swiss participants in this project are supported by Innovate UK (contract no. 10045139) and the Swiss State Secretariat for Education, Research and Innovation (Contract no.22.00123) respectevely.
 """
 from vehicle_dynamics.structures.OutputStates import OutputStates
+from vehicle_dynamics.utils.plot_function import plot_function
 from vehicle_dynamics.structures.Manoeuvre import Manoeuvre
 from vehicle_dynamics.structures.StateVector import StateVector
-from vehicle_dynamics.structures.StaticParameters import StaticParameters
 
-from vehicle_dynamics.utils.plot_function import plot_function
+from vehicle_dynamics.utils.StaticParameters import StaticParameters
 
 from vehicle_dynamics.VehicleDynamics import VehicleDynamics
-
 
 import pandas
 import numpy as np
 import tqdm
 
+from collections import namedtuple
+
+import pickle
+
+with open("Braking.pickle","rb") as handle:
+    data=pickle.load(handle)
 
 frequency = 1000
-end_time = 20
-points = frequency * time
 
-steering = np.linspace(0,1,points)
-throttle = np.ones(points)
-brake = np.zeros(points)
+steering = data["steering"]
+throttle = data["throttle"]
+brake = data["brake"]
 
-time = np.linspace(0, end_time, points)
+points = len(brake)
+end_time = points/frequency
 
-manoeuvre = Manoeuvre(steering, throttle, brake, time)
+time = data["time"]
+
+manoeuvre_M8 = Manoeuvre(steering, throttle, brake, time)
 
 output_states = OutputStates()
+initial_state = StateVector(x = np.array( data["Rel_pos_x"][0]),
+                            y = np.array(data["Rel_pos_y"][0]),
+                            vx = np.array( data["Velocity_X"][0]),
+                            yaw = np.array( data["Yaw"][0]))
 
 car_parameters_path = "bmw_m8.yaml"
 
-vehicle_dynamics = VehicleDynamics(initial_gear = 1, 
+vehicle_dynamics = VehicleDynamics(initial_state = initial_state, 
+                                   initial_gear = 1, 
                                    frequency = frequency, 
                                    car_parameters_path = car_parameters_path)
 
 for i in tqdm.tqdm(range(points)):
     try:
-        output_states.set_states(vehicle_dynamics.tick(*manoeuvre[i]))
+        output_states.set_states(vehicle_dynamics.tick(*manoeuvre_M8[i]))
     except ValueError as e:
         print(e)
-        output_states.padding(len(manoeuvre) - len(output_states))
-        plot_function(output_states, manoeuvre,
+        output_states.padding(len(manoeuvre_M8) - len(output_states))
+        plot_function(output_states, manoeuvre_M8,data,
                       xlim=(-.25,time[-1] + 0.25),
                       plot_type = "powertrain")
         exit()
 
-plot_function(output_states, manoeuvre, 
+plot_function(output_states, manoeuvre_M8,
               xlim=(-.25,time[-1] + 0.25),
-              savefig=True,
-              fig_save_dir="figures/")
+              data=data,
+              savefig=False,
+              fig_save_dir="figures/", 
+              static_parameters = StaticParameters(car_parameters_path, frequency),
+              initial_iteraction = 0)
